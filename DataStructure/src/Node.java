a040c2918b94bdbb42f8edaeade6a63af9471189
LeeSeungYun0225 <67942890+LeeSeungYun0225@users.noreply.github.com>
2022-03-27 00:11:19 +0900

    Add files via upload

--- DataStructure/src/Node.java
+++ DataStructure/src/Node.java
@@ -9,7 +9,7 @@ class Node<T> // 연결 리스트용 노드
 		next = null;
 	}
 	
-	public void printData() // 데이터 출력 테스트를 위한 함수, data의 형태에 따라 수정이 필요함 
+	public void printData() // 데이터 출력 테스트를 위한 함수, data의 형태에 따라 수정이 필요함   ,, 제네릭 형태를 유지하면서 출력형태를 통일하는 방법이 없을까..
 	{
 		System.out.println("Data : " + data);
 	}
